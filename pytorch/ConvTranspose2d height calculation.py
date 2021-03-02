f16f86b5e3e169a9eb03d8d5a87746260a9b4d18
hussein wassouf <57813196+hussein343455@users.noreply.github.com>
2021-03-02 20:15:22 +0300

    Update ConvTranspose2d height calculation.py

--- pytorch/ConvTranspose2d height calculation.py
+++ pytorch/ConvTranspose2d height calculation.py
@@ -7,6 +7,6 @@ padding=0
 output_padding=0
 dilation=1
 
-# By default, stride=1, padding=0, and output_padding=0.
+# By default,dilation=1 stride=1, padding=0, and output_padding=0.
 H_out= (H_in -1)*stride-2*padding+dilation*(kernel_size-1)+output_padding+1
 # H_out = (5-1)*2 - 2*0 + 1*(3-1) + 0 + 1 = 11
