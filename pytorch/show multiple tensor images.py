b2493ea3cc2ccebd94f65b2c829249761e1c7f43
hussein wassouf <57813196+hussein343455@users.noreply.github.com>
2021-03-06 22:09:06 +0300

    Update show multiple tensor images.py

--- pytorch/show multiple tensor images.py
+++ pytorch/show multiple tensor images.py
@@ -8,4 +8,4 @@ def show_tensor_images(image_tensor, num_images=25):
     
 show_tensor_images(data)#data is a tensor with the shape of (60000,1,28,28) or  (60000,3,28,28) 
 #if the data have a shape of (60000,28,28) just add a dimension before calling the function for Example:
-show_tensor_images(only_0.reshape(5923,1,28,28))
+show_tensor_images(data.reshape(5923,1,28,28))
